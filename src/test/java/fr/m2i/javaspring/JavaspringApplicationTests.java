6c412215354ed40b41a3caf759a96ff5e7e0ade3
KylianCrv <kylian.crv@hotmail.com>
2022-08-29 10:09:51 +0200

    first commit

---
+++ src/test/java/fr/m2i/javaspring/JavaspringApplicationTests.java
@@ -0,0 +1,12 @@
+package fr.m2i.javaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
